0ebed23acb80875bccb25d669982b93b7f2fc442
Filippos Papadopoulos <openrisk@outlook.com>
2021-01-21 21:26:37 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# This is a sample Python script using numpymatrix
+
+import numpymatrix as npm
+
+A = npm.matrix([[1, 2], [3, 4]])
+print(A)
+print(A.T)
+
---
+++ tests/test_multiarray.py
@@ -0,0 +1,16 @@
+import numpy as np
+from numpy.testing import assert_, assert_equal, assert_array_equal
+
+class TestView:
+    def test_type(self):
+        x = np.array([1, 2, 3])
+        assert_(isinstance(x.view(npm.matrix), npm.matrix))
+
+    def test_keywords(self):
+        x = np.array([(1, 2)], dtype=[('a', np.int8), ('b', np.int8)])
+        # We must be specific about the endianness here:
+        y = x.view(dtype='<i2', type=npm.matrix)
+        assert_array_equal(y, [[513]])
+
+        assert_(isinstance(y, npm.matrix))
+        assert_equal(y.dtype, np.dtype('<i2'))
